5a5db2605e751af0150ea9dc6c0e2c6c8bdb8dce
João Mauricio <jmauricio@pobox.com>
2016-01-06 11:28:39 -0200

    Usando PHP; Página de produto - Exercício 8.2 com resposta pronta

---
+++ produto.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Mirror Fashion</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+  </head>
+  <body>
+  	<?php include("cabecalho.php"); ?>
+
+	 <div class="produto">
+	      <h1>Fuzzy Cardigan</h1>
+	      <p>por apenas R$ 129,00</p>
+
+	      <form>
+	          <fieldset class="cores">
+	              <legend>Escolha a cor:</legend>
+
+	              <input type="radio" name="cor" value="verde" id="verde" checked>
+	              <label for="verde">
+	                <img src="img/produtos/foto2-verde.png" alt="verde">
+	              </label>
+	              
+	              <input type="radio" name="cor" value="rosa" id="rosa">
+	              <label for="rosa">
+	                <img src="img/produtos/foto2-rosa.png" alt="rosa">
+	              </label>
+	              
+	              <input type="radio" name="cor" value="azul" id="azul">
+	              <label for="azul">
+	                <img src="img/produtos/foto2-azul.png" alt="azul">
+	              </label>
+	              
+	          </fieldset>
+
+	          <input type="submit" class="comprar" value="Comprar">
+	      </form>
+	    </div>
+
+  	
+  	<?php include("rodape.php"); ?>
+  </body>
+</html>
--- sobre.php
+++ sobre.php
@@ -13,14 +13,17 @@
 			<img src="img/logo.png">
 			<h1>A Mirror Fashion</h1>
 	
-			<p>A <strong>Mirror Fashion</strong> é a maior empresa comércio eletrônico no segmento de moda em todo o mundo. 
-			Fundada em 1932, possui filiais em 124 países, sendo líder de mercado com mais de 90% de 
+			<p>A <strong>Mirror Fashion</strong> é a maior empresa comércio eletrônico 
+			no segmento de moda em todo o mundo. Fundada há <?php print date('Y')-1932 ?> anos, 
+			possui filiais em 124 países, sendo líder de mercado com mais de 90% de 
 			participação em 118 deles.</p>
 		</header>
 
 		<article class="container">
 			<section class="centro-distribuicao">
-				<p>Nosso centro de distribuição fica em <a href="https://maps.google.com.br/?q=Jacarezinho">Jacarezinho, no Paraná</a>. De lá, saem 48 aviões que distribuem nossos produtos às casas do mundo todo. Nosso centro de distribuição:</p>
+				<p>Nosso centro de distribuição fica em <a href="https://maps.google.com.br/?q=Jacarezinho">
+				Jacarezinho, no Paraná</a>. De lá, saem 48 aviões que distribuem nossos produtos às casas do 
+				mundo todo. Nosso centro de distribuição:</p>
 				
 				<figure id="centro-distribuicao">
 					<img src="img/centro-distribuicao.png">
