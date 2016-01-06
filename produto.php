24709e8aab033d74547e832947a934452a45490f
João Mauricio <jmauricio@pobox.com>
2016-01-06 14:25:17 -0200

    Exercício 8.13 - Detalhes

--- produto.php
+++ produto.php
@@ -37,18 +37,51 @@
 					<label for="azul">
 						<img src="img/produtos/foto2-azul.png" alt="azul">
 					</label>
-				</fieldset>
+				</fieldset> <!-- fim .cores -->
 
 				<fieldset class="tamanhos">
 					<legend>Escolha o tamanho:</legend>
 
 					<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
-				</fieldset>
+				</fieldset> <!-- fim .tamanhos -->
 				
 				<input type="submit" class="comprar" value="Comprar">
 			</form>
-		</div>
-  	</div>
+		</div> <!-- fim .produto -->
+
+		<div class="detalhes">
+			<h2>Detalhes do produto</h2>
+
+			<p>Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada pelos artesãos da comunidade de Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega a jato.</p>
+
+			<table>
+				<thead>
+					<tr>
+						<th>Característica</th>
+						<th>Detalhe</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Modelo</td>
+						<td>Cardigã 7845</td>
+					</tr>
+					<tr>
+						<td>Material</td>
+						<td>Algodão e poliester</td>
+					</tr>
+					<tr>
+						<td>Cores</td>
+						<td>Azul, Rosa e Verde</td>
+					</tr>
+					<tr>
+						<td>Lavagem</td>
+						<td>Lavar à mão</td>
+					</tr>
+				</tbody>
+			</table>
+		</div> <!-- fim .detalhes -->
+  	</div> <!-- fim .container -->
 
   	
   	<?php include("rodape.php"); ?>
