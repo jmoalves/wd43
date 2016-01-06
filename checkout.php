b864be2a44a9cba00d5b756ce97d7d641514c849
João Mauricio <jmauricio@pobox.com>
2016-01-06 16:56:27 -0200

    Exercício 10.10 - até parte 5

--- checkout.php
+++ checkout.php
@@ -36,6 +36,68 @@
 					</dl>
 				</div> <!-- fim .panel-body -->
 			</div> <!-- fim .panel panel-default -->
+
+
+		<form>
+			<fieldset>
+				<legend>Dados pessoais</legend>
+
+				<div class="form-group">
+					<label for="nome">Nome completo</label>
+					<input type="text" class="form-control" id="nome" name="nome" autofocus>
+				</div>
+
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" class="form-control" id="email" name="email" placeholder="email@exemplo.com">	
+				</div>
+
+				<div class="form-group">
+					<label for="cpf">CPF</label>
+					<input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00">
+				</div>
+
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" value="sim" name="spam" checked>
+						Quero receber spam da Mirror Fashion
+					</label>
+				</div>
+
+			</fieldset>
+
+			<fieldset>
+				<legend>Cartão de crédito</legend>
+				
+				<div class="form-group">
+					<label for="numero-cartao">Número - CVV</label>
+					<input type="text" class="form-control" 
+					       id="numero-cartao" name="numero-cartao">
+				</div>
+
+				<div class="form-group">
+					<label for="bandeira-cartao">Bandeira</label>
+					<select name="bandeira-cartao" id="bandeira-cartao"
+							class="form-control">
+						<option value="master">MasterCard</option>
+						<option value="visa">VISA</option>
+						<option value="amex">American Express</option>
+					</select>
+				</div>
+
+				<div class="form-group">
+					<label for="validade-cartao">Validade</label>
+					<input type="month" class="form-control" 
+						id="validade-cartao" name="validade-cartao">
+				</div>
+			</fieldset>
+
+			<button type="submit" class="btn btn-lg">
+				<span class="glyphicon glyphicon-thumbs-up"></span>
+				Confirmar Pedido
+			</button>
+		</form>
+			
 		</div> <!-- fim .container -->
 	</body>
 </html>
