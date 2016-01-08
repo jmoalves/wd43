fb61becb0794f8a4f6f3a805bb9c783bfe8f4fb3
João Mauricio <jmauricio@pobox.com>
2016-01-08 11:58:46 -0200

    Incorporação do iframe do Google Maps na sobre.php

--- sobre.php
+++ sobre.php
@@ -79,6 +79,8 @@
 			</section>
 		</article>
 
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.4406248452697!2d-43.18303534972095!3d-22.89711324320822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997f5732a12a31%3A0x316901f971660ce1!2sMuseu+do+Amanh%C3%A3!5e0!3m2!1spt-BR!2sbr!4v1452261306159" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
 		<footer>
 			<div class="container">
 				<img src="img/logo-rodape.png" alt="Logo Mirror Fashion">
